ad42852247348ed9f9a06485446f2a64e22a6b61
FredericoSilva015 <fredsilva015@gmail.com>
2017-05-16 16:50:09 +0100

    added new api test site, added select button, changed html

--- solutionController.js
+++ solutionController.js
@@ -1,21 +1,56 @@
 var app = angular.module('app', ['ngResource']);
 
 
-app.factory("api", function($resource) {
-    return $resource("https://reqres.in/api/users?page=1&:id");
-});
-
-app.controller('myCtrl', ['$scope', 'api', function($scope, api) {
-    api.get(function(data) {
-        $scope.table = data;
-        console.log($scope.table);
+app.factory("dados", function($resource) {
+    return $resource('https://jsonplaceholder.typicode.com/posts/:id', {
+        'query': {
+            method: 'GET',
+            isArray: false
+        },
+        'update': {
+            method: 'PUT'
+        }
     });
-    // api.post(function(data) {
-    //     $scope.data = {
-    //         "name": "morpheus",
-    //         "job": "leader"
-    //     };
-    //     console.log($scope.table);
-    // });
+
+});
+// guide for available default actions
+// { 'get':    {method:'GET'},
+//   'save':   {method:'POST'},
+//   'query':  {method:'GET', isArray:true},
+//   'remove': {method:'DELETE'},
+//   'delete': {method:'DELETE'} };
+
+app.controller('myCtrl', ['$scope', 'dados', function($scope, dados) {
+
+    //  update the list of items, it also serves to start up the list
+    $scope.getList = function() {
+        $scope.tempValue = dados.query({
+            id: ''
+        });
+        $scope.tempValue.$promise.then(function(data) {
+            $scope.data = data;
+            // console.log($scope.data);
+        });
+    };
+
+    $scope.id = null;
+    // select one value of the list
+    $scope.selectItem = function() {
+        // id validation
+        if ($scope.id) {
+            $scope.tempValue = dados.get({
+                id: $scope.id
+            });
+            $scope.tempValue.$promise.then(function(data) {
+                //clean the object
+                $scope.data = {};
+                // add the data 1 level lower, so ng-repeat does not break
+                $scope.data.d = data;
+                // console.log($scope.data);
+            });
+        }
+
+    };
+
 
 }]);
